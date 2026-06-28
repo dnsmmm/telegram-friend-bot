2a203af6b953d742c85af4497676fb3f7ebcae22
dnsmmm <daniilsmirnoov@gmail.com>
2026-06-28 20:51:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import TelegramBot from "node-telegram-bot-api";
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const port = process.env.PORT || 3000;
+const appUrl = process.env.RAILWAY_PUBLIC_DOMAIN;
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, { webHook: { port } });
+
+bot.setWebHook(`https://${appUrl}/bot${token}`);
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
@@ -12,4 +16,4 @@ bot.on("message", async (msg) => {
   bot.sendMessage(chatId, `Ты написал: ${msg.text}`);
 });
 
-console.log("🤖 Бот запущен");
+console.log("🤖 Бот запущен через webhook");
